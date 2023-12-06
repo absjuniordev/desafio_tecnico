a7040a5708837423c0ca679518702e79d4affc2f
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-05 21:47:47 -0300

    Criação da função e definição da interação com o usuario

--- desafio_tecnico/bin/desafio_tecnico.dart
+++ desafio_tecnico/bin/desafio_tecnico.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:desafio_tecnico/desafio_tecnico.dart' as desafio_tecnico;
 
-void main(List<String> arguments) {
-  print('Hello world: ${desafio_tecnico.calculate()}!');
+void main() {
+  print("Olá!! ");
+  print("Por favor, insira um numero positivo para a realizaçãodo calculo!");
+  stdout.write("Digite um numero: ");
+  int? num = int.tryParse(stdin.readLineSync().toString());
+  print('O resultado é ${desafio_tecnico.calculate(num!)}!');
 }
