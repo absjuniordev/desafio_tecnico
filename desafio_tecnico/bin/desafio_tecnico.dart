460f2433dddb913a47247038130b1762c21a7b5c
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-05 22:32:36 -0300

    Add READ.ME

--- desafio_tecnico/bin/desafio_tecnico.dart
+++ desafio_tecnico/bin/desafio_tecnico.dart
@@ -3,9 +3,27 @@ import 'dart:io';
 import 'package:desafio_tecnico/desafio_tecnico.dart' as desafio_tecnico;
 
 void main() {
-  print("Olá!! ");
-  print("Por favor, insira um numero positivo para a realizaçãodo calculo!");
-  stdout.write("Digite um numero: ");
-  int? num = int.tryParse(stdin.readLineSync().toString());
-  print('O resultado é ${desafio_tecnico.calculate(num!)}!');
+  int? numero;
+  String opcao = "";
+  do {
+    print("");
+    print(
+        "|-------------------------------------------------------------------|");
+    print(
+        "|----------------------Desafio Técnico Escribo----------------------|");
+    print(
+        "|-------------------------------------------------------------------|");
+    print(
+        "| Por favor, insira um numero positivo para a realizaçãodo calculo! |");
+    stdout.write(" Digite um numero: ");
+    numero = int.tryParse(stdin.readLineSync().toString());
+
+    print(' Resultado :${desafio_tecnico.calculate(numero!)}!');
+    print(
+        "|-------------------------------------------------------------------|");
+    stdout.write(" Deseja realizar outra operação? (S/N): ");
+    opcao = stdin.readLineSync()!;
+  } while (opcao.toUpperCase() != "N");
+
+  print("Obrigado!");
 }
--- desafio_tecnico/lib/desafio_tecnico.dart
+++ desafio_tecnico/lib/desafio_tecnico.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+dynamic calculate(int num) {
+  if (num.isNegative) {
+    return " Valor inserito é negativo";
+  } else {
+    int soma = 0;
+    for (var i = 0; i < num; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        soma = soma + i;
+      }
+    }
+    return soma;
+  }
 }
